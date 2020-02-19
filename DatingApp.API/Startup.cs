4bf361f8ec1ad44407d0555ee9e291824ca20697
Lior Ben Shahar <liorbe@payoneer.com>
2020-02-19 16:43:41 +0200

    FromTerm

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,6 +28,7 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
             services.AddControllers();
         }
 
@@ -42,6 +43,8 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
